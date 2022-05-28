95a9146be85c7edd07ffc2b794ed991ebd580aa8
DK <spol@naver.com>
2022-05-28 17:26:09 +0900

    dataBInding

---
+++ app/src/main/java/com/example/databinding/FirstFragment.kt
@@ -0,0 +1,22 @@
+package com.example.databinding
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.databinding.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+    lateinit var binding: FragmentFirstBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_first, container, false)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/databinding/SecondActivity.kt
@@ -0,0 +1,18 @@
+package com.example.databinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
+import com.example.databinding.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySecondBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_second)
+
+        binding.testTxt
+    }
+}
